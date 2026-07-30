e5e1a308ee60eaf294281622ec50cc5b6c7e4110
andreitopvp2 <andre.ito@outlook.com>
2026-07-30 16:41:45 -0300

    2 portas

--- src/teste-udp.py
+++ src/teste-udp.py
@@ -1,16 +1,23 @@
 import socket
+import select
 
 UDP_IP = "0.0.0.0"
-UDP_PORT = 5005
+UDP_PORT1 = 5005
 UDP_PORT2 = 5006
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock1.bind((UDP_IP, UDP_PORT1))
 
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock2.bind((UDP_IP, UDP_PORT2))
 
+sockets_list = [sock1, sock2]
+
 while True:
-    data, addr = sock.recvfrom(1024)
-    data2, addr2 = sock2.recvfrom(1024)
-    print(data, data2)
+    # O select aguarda até que QUALQUER um dos sockets receba dados
+    readable, _, _ = select.select(sockets_list, [], [])
+
+    for s in readable:
+        data, addr = s.recvfrom(1024)
+        porta = s.getsockname()[1]
+        print(f"Recebido da porta {porta}: {data.decode('utf-8')}")
