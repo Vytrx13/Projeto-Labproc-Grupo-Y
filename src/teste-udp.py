d5a15b986b01051264f13adefaa8abd8ff435e5e
andreitopvp2 <andre.ito@outlook.com>
2026-07-30 16:22:55 -0300

    2 portas

--- src/teste-udp.py
+++ src/teste-udp.py
@@ -2,10 +2,15 @@ import socket
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
+UDP_PORT2 = 5006
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2.bind((UDP_IP, UDP_PORT2))
+
 while True:
     data, addr = sock.recvfrom(1024)
-    print(data)
+    data2, addr2 = sock2.recvfrom(1024)
+    print(data, data2)
